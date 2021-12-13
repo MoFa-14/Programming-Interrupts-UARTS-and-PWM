bd7be80b98441340a899956fff681c8527775215
craig duffy <c-duffy@uwe.ac.uk>
2021-12-13 16:45:12 +0000

    tidied up code

--- Lab5/Src/main.c
+++ Lab5/Src/main.c
@@ -22,12 +22,12 @@ int main(void) {
    
   while(1) {
     while(dutyCycle < __HAL_TIM_GET_AUTORELOAD(&htim2)) {
-      __HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_1, ++dutyCycle);
+      __HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_1, ++dutyCycle); // this can be doe with TIM2->CCR1 = ++dutyCycle; instead
       HAL_Delay(1);
     }
 
     while(dutyCycle > 0) {
-      __HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_1, --dutyCycle);
+      __HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_1, --dutyCycle);// This can be done with TIM2->CCR1 = --dutyCycle;
       HAL_Delay(1);
     }
   }
@@ -92,8 +92,8 @@ void HAL_TIM_PWM_MspInit(TIM_HandleTypeDef* htim_base) {
   if(htim_base->Instance==TIM2) {
     __TIM2_CLK_ENABLE();
     __HAL_RCC_GPIOA_CLK_ENABLE();
-    /**TIM3 GPIO Configuration
-    PA6     ------> TIM3_CH1
+    /**TIM2 GPIO Configuration
+    PA5     ------> TIM2_CH1
     */
     GPIO_InitStruct.Pin = GPIO_PIN_5;
     GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
