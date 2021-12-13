5c08df83d5a5ad1cd38fcf3da01a4e8f0fc571f2
craig duffy <c-duffy@uwe.ac.uk>
2021-12-13 12:16:57 +0000

    added correct GPIO_AF setting

--- Lab5/Src/mainplusputchar.c
+++ Lab5/Src/mainplusputchar.c
@@ -1,11 +1,33 @@
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
+#include <stdio.h>
 
 /* Private variables ---------------------------------------------------------*/
+// pins and clocks for USART
 
+#define DISCOVERY_COM1                          USART1
+#define DISCOVERY_COM1_CLK_ENABLE()             __HAL_RCC_USART1_CLK_ENABLE()
+#define DISCOVERY_COM1_CLK_DISABLE()            __HAL_RCC_USART1_CLK_DISABLE()
+
+#define DISCOVERY_COM1_TX_PIN                   GPIO_PIN_6
+#define DISCOVERY_COM1_TX_GPIO_PORT             GPIOB
+#define DISCOVERY_COM1_TX_GPIO_CLK_ENABLE()     __HAL_RCC_GPIOB_CLK_ENABLE()   
+#define DISCOVERY_COM1_TX_GPIO_CLK_DISABLE()    __HAL_RCC_GPIOB_CLK_DISABLE()  
+#define DISCOVERY_COM1_TX_AF                    GPIO_AF7_USART1
+
+#define DISCOVERY_COM1_RX_PIN                   GPIO_PIN_7
+#define DISCOVERY_COM1_RX_GPIO_PORT             GPIOB
+#define DISCOVERY_COM1_RX_GPIO_CLK_ENABLE()     __HAL_RCC_GPIOB_CLK_ENABLE()   
+#define DISCOVERY_COM1_RX_GPIO_CLK_DISABLE()    __HAL_RCC_GPIOB_CLK_DISABLE()  
+#define DISCOVERY_COM1_RX_AF                    GPIO_AF7_USART1
+
+UART_HandleTypeDef hDiscoUart;
 TIM_HandleTypeDef htim2;
+
 static void SystemClock_Config(void);
-void TIM2_Init(void);
+void BSP_COM_Init( UART_HandleTypeDef *);
+int __io_putchar(int);
+void MX_TIM2_Init(void);
 
 int main(void) {
   HAL_Init();
@@ -13,10 +35,29 @@ int main(void) {
   /* Configure the System clock to have a frequency of 80 MHz */
   SystemClock_Config();
 
-  TIM2_Init();
+  /* Initialize all configured peripherals */
+  hDiscoUart.Instance = DISCOVERY_COM1;
+  hDiscoUart.Init.BaudRate = 115200;
+  hDiscoUart.Init.WordLength = UART_WORDLENGTH_8B;
+  hDiscoUart.Init.StopBits = UART_STOPBITS_1;
+  hDiscoUart.Init.Parity = UART_PARITY_NONE;
+  hDiscoUart.Init.Mode = UART_MODE_TX_RX;
+  hDiscoUart.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  hDiscoUart.Init.OverSampling = UART_OVERSAMPLING_16;
+  hDiscoUart.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
+  hDiscoUart.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
 
-  HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_1);
-  
+  BSP_COM_Init(&hDiscoUart);
+
+   __io_putchar('W');
+
+  MX_TIM2_Init();
+
+  if ( HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_1) == HAL_OK)
+     __io_putchar('S');
+  else
+     __io_putchar('C');
+
   /* Connect a LED to PA5 pin to see the fading effect */
   uint16_t dutyCycle = HAL_TIM_ReadCapturedValue(&htim2, TIM_CHANNEL_1);
    
@@ -24,11 +65,13 @@ int main(void) {
     while(dutyCycle < __HAL_TIM_GET_AUTORELOAD(&htim2)) {
       __HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_1, ++dutyCycle);
       HAL_Delay(1);
+      __io_putchar('u');
     }
 
     while(dutyCycle > 0) {
       __HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_1, --dutyCycle);
       HAL_Delay(1);
+      __io_putchar('d');
     }
   }
 }
@@ -70,8 +113,52 @@ static void SystemClock_Config(void)
   }
 }
 
+/*
+ initialise the COM port
+*/
+
+void BSP_COM_Init(UART_HandleTypeDef *huart)
+{
+  GPIO_InitTypeDef gpio_init_structure;
+
+  /* Enable GPIO RX/TX clocks */
+  DISCOVERY_COM1_TX_GPIO_CLK_ENABLE();
+  DISCOVERY_COM1_RX_GPIO_CLK_ENABLE();
+
+  /* Enable USART clock */
+  DISCOVERY_COM1_CLK_ENABLE();
+
+  /* Configure USART Tx as alternate function */
+  gpio_init_structure.Pin = DISCOVERY_COM1_TX_PIN;
+  gpio_init_structure.Mode = GPIO_MODE_AF_PP;
+  gpio_init_structure.Speed = GPIO_SPEED_FREQ_HIGH;
+  gpio_init_structure.Pull = GPIO_NOPULL;
+  gpio_init_structure.Alternate = DISCOVERY_COM1_TX_AF;
+  HAL_GPIO_Init(DISCOVERY_COM1_TX_GPIO_PORT, &gpio_init_structure);
+
+  /* Configure USART Rx as alternate function */
+  gpio_init_structure.Pin = DISCOVERY_COM1_RX_PIN;
+  gpio_init_structure.Mode = GPIO_MODE_AF_PP;
+  gpio_init_structure.Alternate = DISCOVERY_COM1_RX_AF;
+  HAL_GPIO_Init(DISCOVERY_COM1_RX_GPIO_PORT, &gpio_init_structure);
+
+  /* USART configuration */
+  huart->Instance = DISCOVERY_COM1;
+  HAL_UART_Init(huart);
+}
+
+int __io_putchar(int ch)
+{
+  /* write a character to the serial port and Loop until the end of transmission */
+  while (HAL_OK != HAL_UART_Transmit(&hDiscoUart, (uint8_t *) &ch, 1, 30000))
+  {
+    ;
+  }
+  return ch;
+}
+
 /* TIM2 init function */
-void TIM2_Init(void) {
+void MX_TIM2_Init(void) {
   TIM_OC_InitTypeDef sConfigOC;
 
   htim2.Instance = TIM2;
@@ -102,6 +189,8 @@ void HAL_TIM_PWM_MspInit(TIM_HandleTypeDef* htim_base) {
     GPIO_InitStruct.Alternate = GPIO_AF1_TIM2;
     HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
   }
+  __io_putchar('h');
+
 }
 
 
