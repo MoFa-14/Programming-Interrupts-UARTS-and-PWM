b021f89c3d6e58f3c32865fc8dc691f45ef030b1
Craig Duffy <craig.duffy@uwe.ac.uk>
2021-12-15 12:44:04 +0000

    Update 2leds.c

--- Lab5/Src/2leds.c
+++ Lab5/Src/2leds.c
@@ -1,1 +1,136 @@
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
 
+/* Private variables ---------------------------------------------------------*/
+
+TIM_HandleTypeDef htim1,htim2;
+static void SystemClock_Config(void);
+void TIM_Init(void);
+
+int main(void) {
+  HAL_Init();
+
+  /* Configure the System clock to have a frequency of 80 MHz */
+  SystemClock_Config();
+
+  TIM_Init();
+
+  //HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_1);
+  HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_1);
+  
+  /* Connect a LED to PA5 pin to see the fading effect */
+  //uint16_t dutyCycle = HAL_TIM_ReadCapturedValue(&htim2, TIM_CHANNEL_1);
+  uint16_t dutyCycle = HAL_TIM_ReadCapturedValue(&htim1, TIM_CHANNEL_1);
+   
+  while(1) {
+    while(dutyCycle < __HAL_TIM_GET_AUTORELOAD(&htim1)) {
+      __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_1, ++dutyCycle); // this can be doe with TIM2->CCR1 = ++dutyCycle; instead
+      //TIM1->CCR1 = dutyCycle;
+      HAL_Delay(1);
+    }
+
+    while(dutyCycle > 0) {
+      __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_1, --dutyCycle);// This can be done with TIM2->CCR1 = --dutyCycle;
+      //TIM1->CCR1 = dutyCycle; 
+      HAL_Delay(1);
+    }
+  }
+}
+
+static void SystemClock_Config(void)
+{
+  RCC_ClkInitTypeDef RCC_ClkInitStruct;
+  RCC_OscInitTypeDef RCC_OscInitStruct;
+
+  /* MSI is enabled after System reset, activate PLL with MSI as source */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_MSI;
+  RCC_OscInitStruct.MSIState = RCC_MSI_ON;
+  RCC_OscInitStruct.MSIClockRange = RCC_MSIRANGE_6;
+  RCC_OscInitStruct.MSICalibrationValue = RCC_MSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_MSI;
+  RCC_OscInitStruct.PLL.PLLM = 1;
+  RCC_OscInitStruct.PLL.PLLN = 40;
+  RCC_OscInitStruct.PLL.PLLR = 2;
+  RCC_OscInitStruct.PLL.PLLP = 7;
+  RCC_OscInitStruct.PLL.PLLQ = 4;
+  if(HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    /* Initialization Error */
+    while(1);
+  }
+  
+  /* Select PLL as system clock source and configure the HCLK, PCLK1 and PCLK2 
+     clocks dividers */
+  RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2);
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;  
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;  
+  if(HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_4) != HAL_OK)
+  {
+    /* Initialization Error */
+    while(1);
+  }
+}
+
+/* TIM1/2 init function */
+void TIM_Init(void) {
+  TIM_OC_InitTypeDef sConfigOC1, sConfigOC2;
+
+  /*  htim2.Instance = TIM2;
+  htim2.Init.Prescaler = 499;
+  htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim2.Init.Period = 999;
+  HAL_TIM_PWM_Init(&htim2);
+
+  sConfigOC2.OCMode = TIM_OCMODE_PWM1;
+  sConfigOC2.Pulse = 499;
+  sConfigOC2.OCPolarity = TIM_OCPOLARITY_HIGH;
+  sConfigOC2.OCFastMode = TIM_OCFAST_DISABLE;
+  HAL_TIM_PWM_ConfigChannel(&htim2, &sConfigOC2, TIM_CHANNEL_1); */
+
+  htim1.Instance = TIM15;
+  htim1.Init.Prescaler = 499;
+  htim1.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim1.Init.Period = 999;
+  HAL_TIM_PWM_Init(&htim1);
+
+  sConfigOC1.OCMode = TIM_OCMODE_PWM1;
+  sConfigOC1.Pulse = 499;
+  sConfigOC1.OCPolarity = TIM_OCPOLARITY_HIGH;
+  sConfigOC1.OCFastMode = TIM_OCFAST_DISABLE;
+  HAL_TIM_PWM_ConfigChannel(&htim1, &sConfigOC1, TIM_CHANNEL_1);
+}
+
+void HAL_TIM_PWM_MspInit(TIM_HandleTypeDef* htim_base) {
+  GPIO_InitTypeDef GPIO_InitStruct;
+  /* if(htim_base->Instance==TIM2) {
+    __TIM2_CLK_ENABLE();
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+    TIM2 GPIO Configuration
+    PA5     ------> TIM2_CH1
+    
+    GPIO_InitStruct.Pin = GPIO_PIN_5;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+    GPIO_InitStruct.Alternate = GPIO_AF1_TIM2;
+    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+   
+    } */
+  if(htim_base->Instance==TIM15) {
+    __HAL_RCC_GPIOB_CLK_ENABLE();
+    __TIM15_CLK_ENABLE();
+    GPIO_InitStruct.Pin = GPIO_PIN_14;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+    GPIO_InitStruct.Alternate = GPIO_AF14_TIM15;
+    HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+    }
+}
+
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
